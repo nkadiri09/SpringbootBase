a3debe81c2cadf1e3a865306a7353838960e8df5
narendra Kadiri <nkadiri09@gmail.com>
2020-01-05 21:10:50 -0600

    Added different profile

--- src/main/java/com/example/SpringbootBase/config/MessageProperties.java
+++ src/main/java/com/example/SpringbootBase/config/MessageProperties.java
@@ -4,7 +4,9 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Profile("dev")
 @Setter @Getter
 @Configuration
 @ConfigurationProperties(prefix = "message.default")
