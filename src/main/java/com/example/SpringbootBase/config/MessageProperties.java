530ce0cbd8fa23d1f2c1084652d3f1a7c28d4ecc
narendra Kadiri <nkadiri09@gmail.com>
2020-01-06 23:10:53 -0600

    added qa properties

--- src/main/java/com/example/SpringbootBase/config/MessageProperties.java
+++ src/main/java/com/example/SpringbootBase/config/MessageProperties.java
@@ -6,7 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Profile("dev")
+@Profile({"qa","dev"})
 @Setter @Getter
 @Configuration
 @ConfigurationProperties(prefix = "message.default")
