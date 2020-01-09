4344d3046ed0d2217d9927f93dd19071ff32ebb3
narendra Kadiri <nkadiri09@gmail.com>
2020-01-08 23:20:05 -0600

    added swagger

--- src/main/java/com/example/SpringbootBase/SpringbootBaseApplication.java
+++ src/main/java/com/example/SpringbootBase/SpringbootBaseApplication.java
@@ -2,13 +2,14 @@ package com.example.SpringbootBase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnabbleSwagger2
 public class SpringbootBaseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBaseApplication.class, args);
 	}
-
 
 }
