8db3c44daf992b8d7937403c524496492b9e643d
Narendra <nkadiri09@gmail.com>
2020-01-22 23:27:26 -0600

    config chages

--- src/main/java/com/example/SpringbootBase/SpringbootBaseApplication.java
+++ src/main/java/com/example/SpringbootBase/SpringbootBaseApplication.java
@@ -2,10 +2,7 @@ package com.example.SpringbootBase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @SpringBootApplication
-@EnabbleSwagger2
 public class SpringbootBaseApplication {
 
 	public static void main(String[] args) {
